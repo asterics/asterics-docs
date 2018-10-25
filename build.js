3ba5fc1940f1caf98ecb4079f201f96656384431
Alija Sabic <sabic.alija@gmail.com>
2018-10-26 00:51:20 +0200

    Implement acs-help files copy, conversion, and terminal info

--- build.js
+++ build.js
@@ -2,10 +2,18 @@ const { Checkout, Repository, Tag, Tree } = require("nodegit");
 const path = require("path");
 const fs = require("fs-extra");
 const klaw = require("klaw-sync");
+const turndown = require("turndown")();
+const ora = require("ora");
 
 let asterics = __dirname + "/source/asterics";
 let acs_help = asterics + "/Documentation/ACS-Help/HTML";
 let acs_help_intermediate = __dirname + "/intermediate/help";
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
 
 (async () => {
   let astericsRepo = await Repository.open(asterics);
@@ -16,35 +24,73 @@ let acs_help_intermediate = __dirname + "/intermediate/help";
   astericsTags.forEach(tag => {
     if (/v\d+.\d+$/.test(tag)) astericsVersions.push(tag);
   });
-  console.log(astericsVersions);
 
-  /* Checkout versions */
-  let tag = await astericsRepo.getTagByName(astericsVersions[0]);
-  console.log(tag.targetId().tostrS());
-  await Checkout.tree(astericsRepo, tag.targetId().tostrS(), {
-    checkoutStrategy: Checkout.STRATEGY.SAFE_CREATE
-  });
-  await astericsRepo.setHeadDetached(tag.targetId().tostrS(), astericsRepo.defaultSignature);
-
-  /* Copy plugin docs from Documentation */
-  let dirs = await klaw(acs_help, { nofile: true });
-  // let files = await klaw(acs_help, { nodir: true });
-  console.log(dirs);
-  // console.log(files);
-
+  /* Ensure target folder */
   await fs.emptyDir(acs_help_intermediate);
 
-  dirs.forEach(dir => {
-    let diff = path.relative(acs_help, dir.path);
-    let f = `${acs_help_intermediate}/${astericsVersions[0]}/${diff}`;
-    if (!f.match(/stylesheet/) && !f.match(/img/)) {
-      console.log(f);
-      await fs.ensureDir(f)
+  /* Checkout versions */
+  await asyncForEach(astericsVersions, async version => {
+    let tag, targetid;
+    let spinner = ora(`check out tag ${version}`).start();
+
+    try {
+      tag = await astericsRepo.getTagByName(version);
+      targetid = tag.targetId().tostrS();
+    } catch (err) {
+      try {
+        tag = await astericsRepo.getReferenceCommit(`refs/tags/${version}`);
+        targetid = tag.id().tostrS();
+      } catch (err) {
+        console.log(err);
+      }
     }
+    // console.log(tag);
+    await Checkout.tree(astericsRepo, targetid, {
+      checkoutStrategy: Checkout.STRATEGY.SAFE_CREATE
+    });
+    await astericsRepo.setHeadDetached(targetid, astericsRepo.defaultSignature);
 
+    /* Copy plugin docs from Documentation */
+    spinner.text = `copy and convert files of ${version}`;
+    /* Get all folders */
+    let dirs = await klaw(acs_help, { nofile: true });
+    // let files = await klaw(acs_help, { nodir: true });
+
+    await asyncForEach(dirs, async dir => {
+      let rel = path.relative(acs_help, dir.path);
+      let dest = `${acs_help_intermediate}/${version}/${rel}`;
+
+      if (dest.match(/img/)) {
+        fs.copySync(dir.path, dest);
+      }
+
+      if (!dest.match(/(stylesheet|img)/)) {
+        /* Ensure target subfolder exists */
+        await fs.ensureDir(dest);
+
+        /* Read all files from source folder */
+        let files = fs.readdirSync(dir.path);
+
+        await asyncForEach(files, async file => {
+          if (file.match(/.*html?$/)) {
+            let dstMD = file.substr(0, file.lastIndexOf(".")) + ".md";
+            let src = `${dir.path}/${file}`;
+            let dstHTML = `${dest}/${file}`;
+            dstMD = `${dest}/${dstMD}`;
+
+            let text = fs.readFileSync(src);
+            text = text.toString("utf8");
+            let mtext = turndown.turndown(text);
+
+            fs.copySync(src, dstHTML);
+            fs.writeFile(dstMD, mtext);
+          }
+        });
+      }
+    });
+
+    spinner.succeed(`${version}`);
   });
-
-
   // let tree = await Checkout.tree(astericsRepo, tag.targetId(), {
   //   checkoutStrategy: Checkout.STRATEGY.SAFE_CREATE
   // });
@@ -63,7 +109,8 @@ let acs_help_intermediate = __dirname + "/intermediate/help";
 
   /* Reset to master branch */
   let commit = await astericsRepo.getReferenceCommit("master");
-  console.log(commit.id().tostrS());
+  // console.log(commit.id().tostrS());
+  console.log("checking out master branch");
   await astericsRepo.setHeadDetached(commit.id(), astericsRepo.defaultSignature);
   await astericsRepo.checkoutBranch("master");
 })().catch(err => {
