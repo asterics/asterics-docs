afa2601cf34614ef55fe5bd3aef0009c1f99aa50
Alija Sabic <sabic.alija@gmail.com>
2019-01-15 01:43:24 +0100

    prebuild: add npm module asterics-web-git

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,58 +1,46 @@
 require("dotenv").config();
-const fs = require("fs");
+const { gitLocalPath } = require("asterics-web-git");
 const shell = require("shelljs");
 
 if (!shell.which("git")) {
-  shell.echo("Sorry, this script reqiuire git");
+  shell.echo("Sorry, this script requires git");
   shell.exit(1);
 }
 
-let asterics = {
-    path: process.env.GIT_SUBMODULE_ASTERICS,
-    ref: process.env.GIT_SUBMODULE_ASTERICS_REF,
-    branch: process.env.GIT_SUBMODULE_ASTERICS_BRANCH || "master",
-    fatal: process.env.GIT_SUBMODULE_ASTERICS_FATALITY || false
-  },
-  asterics_wiki = {
-    path: process.env.GIT_SUBMODULE_ASTERICS_WIKI,
-    ref: process.env.GIT_SUBMODULE_ASTERICS_WIKI_REF,
-    branch: process.env.GIT_SUBMODULE_ASTERICS_WIKI_BRANCH || "master",
-    fatal: process.env.GIT_SUBMODULE_ASTERICS_WIKI_FATALITY || false
-  };
-// commands_init = [
-//   r => {
-//     return {
-//       cmd: `git submodule init ${r.path}`,
-//       response: `failed initializing submodule ${r.path}`
-//     };
-//   },
-//   r => {
-//     return {
-//       cmd: `git submodule update ${r.path}`,
-//       response: `failed cloning submodule ${r.path}`
-//     };
-//   },
-//   r => {
-//     return {
-//       cmd: `git --git-dir=${r.path} --work-tree=${r.path} checkout ${
-//         r.branch
-//       }`,
-//       response: `failed checking out ${r.branch} of submodule ${r.path}`
-//     };
-//   }
-// ];
+var asterics = {
+  path: process.env.GIT_SUBMODULE_ASTERICS,
+  ref: gitLocalPath(__dirname, process.env.GIT_SUBMODULE_ASTERICS_REF),
+  branch: process.env.GIT_SUBMODULE_ASTERICS_BRANCH || "master",
+  fatal: process.env.GIT_SUBMODULE_ASTERICS_FATALITY || false
+};
+var asterics_wiki = {
+  path: process.env.GIT_SUBMODULE_ASTERICS_WIKI,
+  ref: gitLocalPath(__dirname, process.env.GIT_SUBMODULE_ASTERICS_WIKI_REF),
+  branch: process.env.GIT_SUBMODULE_ASTERICS_WIKI_BRANCH || "master",
+  fatal: process.env.GIT_SUBMODULE_ASTERICS_WIKI_FATALITY || false
+};
 
-/* Run init commands for each repository */
+/* Run init commands for (all) repositories */
 [asterics, asterics_wiki].forEach(r => {
-  /* Clone from local repository (if folder exists). FIXME: search .git/ folder */
-  let ref = fs.existsSync(r.ref[0] === "/" ? r.ref : `../${r.ref}`) ? `--reference ${r.ref}` : "";
-  [
-    `git submodule update --init ${ref} ${r.path}`,
-    `git --git-dir=${r.path}/.git --work-tree=${r.path} checkout ${r.branch}`
-  ].forEach(cmd => {
-    if (shell.exec(cmd).code !== 0) {
-      shell.echo(``);
-      if (r.fatal) shell.exit(1);
-    }
-  });
+  let ref = r.ref ? `--reference ${r.ref}` : "";
+  if (ref) console.log(`From (local) ${r.ref}`);
+
+  /* clone repository */
+  // eslint-disable-next-line
+  if (shell.exec(`git submodule update --init ${ref} ${r.path}`).code !== 0) {
+    shell.echo(`failed cloning ${r.path}`);
+    if (r.fatal) shell.exit(1);
+  }
+  /* checkout branch/tag */
+  // eslint-disable-next-line
+  if (shell.exec(`git --git-dir=${r.path}/.git --work-tree=${r.path} checkout ${r.branch}`).code !== 0) {
+    shell.echo(`failed checking out ${r.branch} in ${r.path}`);
+    if (r.fatal) shell.exit(1);
+  }
+  /* pull latest commits */
+  // eslint-disable-next-line
+  if (shell.exec(`git --git-dir=${r.path}/.git --work-tree=${r.path} pull origin ${r.branch}`).code !== 0) {
+    shell.exec(`failed to pull latest commits of ${r.path}`);
+    if (r.fatal) shell.exit(1);
+  }
 });
