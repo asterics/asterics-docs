99bc5c91552a139d5c1c17a711b6f90af5dbc23c
Martin Deinhofer <deinhofe@technikum-wien.at>
2019-03-15 10:10:58 +0100

    added copying of enhanceApp.js from src/config to docs/.vuepress cleaned up enhanceApp.js which is needed to enable bootstrap-vue functionality

--- src/config/enhanceApp.js
+++ src/config/enhanceApp.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 
-// Vue.use(BootstrapVue);
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
