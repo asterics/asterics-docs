39b316235d5dfeb38dfe69533602c3e1148e2365
Martin Deinhofer <deinhofe@technikum-wien.at>
2019-03-28 17:16:07 +0100

    added 'Learn More' button moved footer with 'Start', 'Edit',... button group to location below image

--- src/components/ModelTile.vue
+++ src/components/ModelTile.vue
@@ -5,24 +5,24 @@
         <b-col md="6">
           <b-embed v-if="isVideo" type="iframe" aspect="16by9" :src="imageUrl" allowfullscreen/>
           <b-card-img v-else :src="$withBase(imageUrl)" class="rounded-0"/>
+          <b-card-footer>
+            <div>
+              <b-button-group>
+                <div v-for="buttonElem in buttons">
+                  <b-button :href="$withBase(buttonElem.href)" variant="primary">{{buttonElem.text}}</b-button>
+                </div>
+              </b-button-group>
+            </div>
+          </b-card-footer>
         </b-col>
         <b-col md="6">
           <b-card-body :title="title">
             <div v-for="tag in tags">
               <b-badge :href="$withBase(tag.href)" variant="primary">{{tag.text}}</b-badge>
             </div>
-
             <b-card-text>{{shortDesc}}</b-card-text>
+            <b-button :href="$withBase(target)" variant="primary">Learn More</b-button>
           </b-card-body>
-          <b-card-footer>
-            <div>
-              <b-button-group>
-                <div v-for="buttonElem in buttons">
-                  <b-button variant="primary">{{buttonElem.text}}</b-button>
-                </div>
-              </b-button-group>
-            </div>
-          </b-card-footer>
         </b-col>
       </b-row>
     </b-card>
