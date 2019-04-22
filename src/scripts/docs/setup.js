cd65d19b61c5beb8be16b8cf6dd62735d48fb8f7
Alija Sabic <sabic.alija@gmail.com>
2019-04-23 01:58:32 +0200

    yarn docs setup: remove comments

--- src/scripts/docs/setup.js
+++ src/scripts/docs/setup.js
@@ -27,25 +27,6 @@ export const handler = async options => {
   options.verbose = config.get("verbose") || options.verbose;
   const index = await loadIndex(options);
   await load(index, options);
-
-  // await load(index, version, version === versions[versions.length - 1] ? "" : version, options);
-
-  // if (version === undefined) {
-  // console.log("use config");
-  // }
-  // Resolved dependencies
-  // (async () => {
-  //   let index = [];
-  //   for (let version of config.get("versions")) {
-  //     await resolve(index, version);
-  //   }
-  //   console.log(index);
-  // Create index
-  // Add routes
-  // Add jobs
-  // Create output folder
-  // Process index
-  // })();
 };
 
 async function loadIndex(options) {
@@ -62,24 +43,6 @@ async function loadIndex(options) {
         const c = await r.getBranchCommit(`origin/${version}`);
         const e = await c.getEntry(cfg.source);
         const t = await e.getTree();
-        // const f = await walk(cfg.filter, t, async file => {
-        //   let destination = relative(cfg.source, file);
-
-        //   if (version !== versions[versions.length - 1]) {
-        //     destination = join(version, destination);
-        //   }
-
-        //   if (index[destination]) {
-        //     console.log(`error: path ${destination} exists already`);
-        //   } else {
-        //     index[destination] = {
-        //       repository: cfg.repository,
-        //       source: cfg.source,
-        //       branch: version,
-        //       hash: c.sha()
-        //     };
-        //   }
-        // });
 
         const f = await walk(cfg.source, cfg.filter, t);
         for (let file of f) {
@@ -113,7 +76,6 @@ async function load(index, options) {
     const { repository, source, branch, hash } = index[destination];
     const { location } = config.get("submodules").find(s => s.name === repository);
     const d = join(process.cwd(), config.get("documentation"), destination);
-    const s = join(location, source);
 
     try {
       const r = await Repository.open(join(process.cwd(), location));
@@ -134,23 +96,11 @@ async function load(index, options) {
         console.log(`creating text file: ${d}`);
         fs.writeFileSync(d, b.toString());
       }
-
-      // console.log(`copying ...\n \t ${s}\n \t ${d}`);
-    } catch (err) {}
+    } catch (err) {
+      console.log("error", location, source, filter);
+    }
   }
 }
-
-// async function walk(filter, tree, cb) {
-//   await tree
-//     .walk()
-//     .on("end", trees => {
-//       trees = trees.filter(t => t && filter.test(t.path()));
-//       for (let tree of trees) {
-//         cb(tree.path());
-//       }
-//     })
-//     .start();
-// }
 
 async function walk(location, filter, tree) {
   return await new Promise(async resolve => {
@@ -168,130 +118,10 @@ async function walk(location, filter, tree) {
   });
 }
 
-//
-//
-//
-//
-
-// async function loadIndex(version, options) {
-//   let files = {};
-//   for (let dep of config.get("dependencies")) {
-//     await loadDependency(files, version, dep, options);
-//   }
-//   return { files, version };
-// }
-
-async function loadDependency(files, version, dependency, options) {
-  const cfg = getDependencyConfiguration(dependency);
-  const { location } = config.get("submodules").find(s => s.name === cfg.repository);
-
-  try {
-    const r = await Repository.open(join(process.cwd(), location));
-    const c = await r.getBranchCommit(`origin/${version}`);
-    const e = await c.getEntry(cfg.source);
-    const t = await e.getTree();
-    const f = await getFiles(cfg.source, cfg.filter, t);
-    files[cfg.repository] = files[cfg.repository] || [];
-    files[cfg.repository].push({
-      source: cfg.source,
-      destination: dependency.destination,
-      files: f
-    });
-  } catch (err) {
-    console.log("error", location, version, cfg.source, cfg.filter);
-  }
-}
-
 function getDependencyConfiguration(dependency) {
   let s = dependency.source.split(":");
   const repository = s.length === 2 ? s[0] : dependency.repository;
   const source = s.length === 2 ? s[1] : dependency.source;
   const filter = dependency.filter ? dependency.filter : /.*/;
   return { repository, source, filter };
-}
-
-async function getFiles(location, filter, tree) {
-  return await new Promise(async resolve => {
-    let a = [];
-    await tree
-      .walk()
-      .on("end", trees => {
-        trees = trees.filter(t => filter.test(t.path()));
-        for (let tree of trees) {
-          a.push(relative(location, tree.path()));
-        }
-        resolve(a);
-      })
-      .start();
-  });
-}
-
-async function loadFiles(index) {
-  console.log(index);
-}
-
-//
-//
-//
-//
-async function resolve(index, version) {
-  let files = {};
-
-  for (let dependency of config.get("dependencies")) {
-    const { submodule } = processSource(dependency);
-    files[submodule] = files[submodule] || [];
-    await load(files[submodule], dependency, version);
-  }
-
-  index.push({ version, files });
-}
-
-// async function load(files, dependency, version) {
-//   const { submodule, srcPath } = processSource(dependency);
-//   const { location } = config.get("submodules").find(s => s.name === submodule);
-//   for (let f of await loadFilesFrom(location, version, srcPath, dependency.filter)) {
-//     files.push({
-//       source: join(srcPath, f),
-//       destination: join(dependency.destination, f)
-//     });
-//   }
-//   // console.log(files);
-// }
-
-function processSource({ repository, source }) {
-  let m = /(.*):(.*)/.exec(source);
-  return { submodule: m ? m[1] : repository, srcPath: m ? m[2] : source };
-}
-
-async function loadFilesFrom(location, version, source, filter) {
-  try {
-    const repository = await Repository.open(join(process.cwd(), location));
-    const commit = await repository.getBranchCommit(`origin/${version}`);
-    const entry = await commit.getEntry(source);
-    const tree = await entry.getTree();
-    return await getFiles(source, filter, tree);
-  } catch (err) {
-    return [];
-  }
-}
-
-async function loadSource(index, submodule, source, filter) {
-  /* Get handle to git repository */
-  const { location } = config.get("submodules").find(s => s.name === submodule);
-  // console.log(location);
-
-  // (async () => {
-  repository = await Repository.open(join(process.cwd(), location));
-  // console.log(repository);
-
-  for (let version of config.get("versions")) {
-    // console.log(version);
-
-    let commit = await repository.getBranchCommit(`origin/${version}`);
-    let entry = await commit.getEntry(source);
-    let tree = await entry.getTree();
-    let result = await getFiles(source, filter, tree);
-    // console.log(result);
-  }
-  // })();
 }
