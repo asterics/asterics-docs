24f569ce625c84eda724062a14c4ac6f9c9c2555
Alija Sabic <sabic.alija@gmail.com>
2019-04-23 15:00:51 +0200

    Add new components

---
+++ src/vuepress/components/FundingGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <Group style="margin: auto;">
+    <Partner
+      image="https://embsys.technikum-wien.at/projects/studyathome-intl/studyAThome_noir.svg"
+      alt="StudyATHome Internationally - Project Logo"
+    />
+    <Partner
+      image="https://embsys.technikum-wien.at/projects/studyathome-intl/studyAThome_noir.svg"
+      alt="MA23, City of Vienna - Logo"
+    />
+    <Partner
+      image="https://embsys.technikum-wien.at/projects/studyathome-intl/studyAThome_noir.svg"
+      alt="ToRaDes - Project Logo"
+    />
+    <Partner
+      image="https://embsys.technikum-wien.at/projects/studyathome-intl/studyAThome_noir.svg"
+      alt="UAS Technikum Wien - Logo"
+    />
+    <Partner
+      image="https://embsys.technikum-wien.at/projects/studyathome-intl/studyAThome_noir.svg"
+      alt="KI-I"
+    />
+  </Group>
+</template>
+
+<script>
+import Group from "./Group.vue";
+import Partner from "./Partner.vue";
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/vuepress/components/Group.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <slot></slot>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
