71f957a1ac5ceb1a11cd7ae79db0a6e7a24587c5
Alija Sabic <sabic.alija@gmail.com>
2019-04-24 15:08:44 +0200

    Update start page

--- src/vuepress/themes/vuepress-theme-docs/components/Jumbo.vue
+++ src/vuepress/themes/vuepress-theme-docs/components/Jumbo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
+    <br>
     <!-- <b-jumbotron class="jumbo" bg-variant="info" border-variant="dark" text-variant="white" fluid> -->
-    <b-jumbotron class="jumbo" text-variant="white" fluid>
-      <b-img class="logo" left src="/assets/img/asterics-logo.svg" fluid alt="AsTeRICS Logo"></b-img>
+    <b-jumbotron class="jumbo" text-variant="black" fluid>
       <template slot="header">
         <span class="jumbo-header">AsTeRICS</span>
       </template>
@@ -11,10 +11,11 @@
       >Create Customized Low-Cost Assistive Technologies for People with Disabilities.</template>
 
       <p>Are you disabled and want to use a computer to work, browse the internet or write E-Mails?</p>
-      <b-button-group>
-        <b-button variant="primary" href="./solutions/">Discover Solutions</b-button>
-        <b-button variant="info" href="get-started/">Get Started</b-button>
-      </b-button-group>
+      <!-- <b-button-group> -->
+      <b-button variant="primary" href="./solutions/">Discover Solutions</b-button>
+      <b-button variant="info" href="get-started/">Get Started</b-button>
+      <!-- </b-button-group> -->
+      <!-- <b-img class="logo" left src="/assets/img/asterics-logo.svg" fluid alt="AsTeRICS Logo"></b-img> -->
     </b-jumbotron>
   </div>
 </template>
@@ -37,13 +38,14 @@ export default {};
   // background: #075069;
   // background: linear-gradient(215deg, #0ea1d2, #075069);
   height: 400px;
-  background-color: #0ea1d2;
+  // background-color: #0ea1d2;
+  background-color: rgb(233, 236, 239);
   // background-color: #075069;
   // background-image: linear-gradient(120deg, #075069, #0ea1d2 20%);
 }
 
 .jumbo-header {
-  // font-family: 'Open Sans';
+  font-family: 'Open Sans';
   // font-family: 'ZCOOL KuaiLe';
   // font-family: 'Roboto';
   // font-family: 'Pacifico';
@@ -58,22 +60,33 @@ export default {};
   // font-family: 'Viga';
   // font-family: 'Amaranth';
   // font-family: 'Carter One';
-  font-family: 'Baloo';
+  // font-family: 'Baloo';
 }
 
 .logo {
   position: absolute;
-  z-index: 0;
-  right: 0;
+  // left: -60px;
+  right: 60px;
+  top: 80px;
+  height: 400px;
+  width: 35%;
+  z-index: 1;
+  visibility: hidden;
+  // visibility: hidden;
+  // position: absolute;
+  // z-index: 0;
+  // top: 90px;
+  // left: -100px;
   // width: 60%;
 }
 
-@media screen and (max-width: 1060px) {
+@media screen and (max-width: 1300px) {
   .logo {
-    margin-top: 5%;
-    width: 55%;
-    transition: width 0.5s ease-out;
-    transition: margin 0.5s ease-out;
+    visibility: hidden;
+    // margin-top: 5%;
+    // width: 55%;
+    // transition: width 0.5s ease-out;
+    // transition: margin 0.5s ease-out;
   }
 }
 </style>
