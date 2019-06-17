67a2de7b3641c6ffbf8cfd709f8fbf11cbbb4413
Alija Sabic <sabic.alija@gmail.com>
2019-06-17 14:24:15 +0200

    Update configuration, add new parameters, add configuration in file .env

--- src/config/config.js
+++ src/config/config.js
@@ -10,167 +10,84 @@ const convict = require("convict"),
 
 /* Load configuration */
 config.load({
-  // versions: ["2.3", "2.5", "2.6", "2.7", "2.8", "3.0", "3.0.1", "pre-3.1"],
-  versions: ["2.6", "2.7", "2.8", "3.0", "3.0.1", "pre-3.1"],
-  // versions: ["pre-3.1"],
+  versions: ["2.6", "2.7", "2.8", "3.0", "3.0.1", "4.0"],
   submodules: [
+    {
+      name: "asterics-docs",
+      location: "src/external/asterics-docs",
+      reference: getReferenceInPath(process.cwd(), "../asterics-docs")
+    },
     {
       name: "AsTeRICS",
       location: "src/external/asterics",
-      reference: getReferenceInPath(process.cwd(), "AsTeRICS"),
-      branch: "master"
+      reference: getReferenceInPath(process.cwd(), "AsTeRICS")
     },
     {
       name: "WebACS",
       location: "src/external/webacs",
-      reference: getReferenceInPath(process.cwd(), "WebACS"),
-      branch: "master"
+      reference: getReferenceInPath(process.cwd(), "WebACS")
     },
     {
       name: "AsTeRICS.wiki",
       location: "src/external/asterics-wiki",
-      reference: getReferenceInPath(process.cwd(), "AsTeRICS.wiki"),
-      branch: "master"
+      reference: getReferenceInPath(process.cwd(), "AsTeRICS.wiki")
     }
   ],
   dependencies: [
     {
+      repository: "asterics-docs",
+      source: "src/vuepress",
+      destination: ".vuepress",
+      filter: /\.*/,
+      branch: "alija/feature/workflow"
+    },
+    {
       source: "AsTeRICS:Documentation/docs",
       destination: "",
-      filter: /\.(md|jpg|png|svg)$/i
+      filter: /\.(md|jpg|png|svg)$/i,
+      versions: [["2.6", "2.6"], ["2.7", "2.7"], ["2.8", "2.8"], ["3.0", "3.0"], ["3.0.1", "3.0.1"], ["4.0", "pre-3.1"]]
     },
     {
       repository: "AsTeRICS",
       source: "Documentation/docs",
       destination: "",
-      filter: /\.(md|jpg|png|svg)$/i
+      filter: /\.(md|jpg|png|svg)$/i,
+      versions: [["2.6", "2.6"], ["2.7", "2.7"], ["2.8", "2.8"], ["3.0", "3.0"], ["3.0.1", "3.0.1"], ["4.0", "pre-3.1"]]
     },
     {
       source: "AsTeRICS:Documentation/ACS-Help/HTML/ACS",
       destination: "manuals/ACS",
-      filter: /\.(html?|jpg|png|svg)$/i
+      filter: /\.(html?|jpg|png|svg)$/i,
+      versions: [["2.6", "2.6"], ["2.7", "2.7"], ["2.8", "2.8"], ["3.0", "3.0"], ["3.0.1", "3.0.1"], ["4.0", "pre-3.1"]]
     },
     {
       source: "AsTeRICS:Documentation/ACS-Help/HTML/ACCS",
       destination: "manuals/ACS",
-      filter: /\.(html?|jpg|png|svg)$/i
+      filter: /\.(html?|jpg|png|svg)$/i,
+      versions: [["2.6", "2.6"], ["2.7", "2.7"], ["2.8", "2.8"], ["3.0", "3.0"], ["3.0.1", "3.0.1"], ["4.0", "pre-3.1"]]
     },
     {
       repository: "WebACS",
       source: "help_files/WebACS",
       destination: "manuals/WebACS",
-      filter: /\.(html?|jpg|png|svg)$/i
+      filter: /\.(html?|jpg|png|svg)$/i,
+      branch: "master"
+      // versions: [["2.6", "2.6"], ["2.7", "2.7"], ["2.8", "2.8"], ["3.0", "3.0"], ["3.0.1", "3.0.1"], ["4.0", "pre-3.1"]]
     }
   ]
-  // dependency: [
-  //   {
-  //     repository: "AsTeRICS",
-  //     source: "Documentation/docs",
-  //     destination: config.get("documentation"),
-  //     recurse: true,
-  //     filter: /\.(md|jpg|png|svg)$/i,
-  //     process: [{ rule: /\.(md|jpg|png|svg)$/i, apply: "copy" }],
-  //     postprocess: [{ rule: /\.md$/i, apply: ["edit-link"] }],
-  //     map: {
-  //       "2.3": "pre-3.1",
-  //       "2.5": "pre-3.1",
-  //       "2.6": "pre-3.1",
-  //       "2.7": "pre-3.1",
-  //       "2.8": "pre-3.1",
-  //       "3.0": "pre-3.1",
-  //       "3.0.1": "pre-3.1",
-  //       "3.1": "pre-3.1"
-  //     }
-  //   },
-  //   {
-  //     repository: "AsTeRICS",
-  //     source: "Documentation/ACS-Help/HTML/ACS",
-  //     destination: config.get("documentation") + "/manuals/ACS",
-  //     recurse: true,
-  //     filter: /\.(html?|jpg|png|svg)$/i,
-  //     process: [
-  //       { rule: /\.html?$/i, apply: "html-to-markdown-copy" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "lowercase" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "copy" }
-  //     ],
-  //     postprocess: [
-  //       {
-  //         rule: /\.md$/i,
-  //         apply: ["remove-first-two-lines", "correct-image-path", "lowercase-image", "edit-link"]
-  //       }
-  //     ],
-  //     map: {}
-  //   },
-  //   {
-  //     repository: "AsTeRICS",
-  //     source: "Documentation/ACS-Help/HTML/ARE",
-  //     destination: config.get("documentation") + "/manuals/ARE",
-  //     recurse: true,
-  //     filter: /\.(html?|jpg|png|svg)$/i,
-  //     process: [
-  //       { rule: /\.html?$/i, apply: "html-to-markdown-copy" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "lowercase" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "copy" }
-  //     ],
-  //     postprocess: [
-  //       {
-  //         rule: /\.md$/i,
-  //         apply: ["remove-first-two-lines", "correct-image-path", "lowercase-image", "edit-link"]
-  //       }
-  //     ],
-  //     map: {}
-  //   },
-  //   {
-  //     repository: "AsTeRICS",
-  //     source: "Documentation/ACS-Help/HTML/Plugins",
-  //     destination: config.get("documentation") + "/plugins",
-  //     recurse: true,
-  //     filter: /\.(html?|jpg|png|svg)$/i,
-  //     process: [
-  //       { rule: /\.html?$/i, apply: "html-to-markdown-copy" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "lowercase" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "copy" }
-  //     ],
-  //     postprocess: [
-  //       {
-  //         rule: /\.md$/i,
-  //         apply: ["remove-first-two-lines", "correct-image-path", "lowercase-image", "edit-link"]
-  //       }
-  //     ],
-  //     map: {}
-  //   },
-  // {
-  //   repository: "AsTeRICS",
-  //   source: "Documentation/docs/.vuepress/public",
-  //   destination: "public",
-  //   recurse: true,
-  //   process: [{ rule: /\.*$/i, apply: "copy" }],
-  //   postprocess: [],
-  //   map: {}
-  // },
-  //   {
-  //     repository: "WebACS",
-  //     source: "help_files/WebACS",
-  //     destination: config.get("documentation") + "/manuals/WebACS",
-  //     recurse: true,
-  //     filter: /\.(html?|jpg|png|svg)$/i,
-  //     process: [
-  //       { rule: /\.html?$/i, apply: "html-to-markdown-copy" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "lowercase" },
-  //       { rule: /\.(jpg|png|svg)$/i, apply: "copy" }
-  //     ],
-  //     postprocess: [
-  //       {
-  //         rule: /\.md$/i,
-  //         apply: ["remove-first-two-lines", "correct-image-path", "lowercase-image", "edit-link"]
-  //       }
-  //     ],
-  //     map: {}
-  //   }
-  // ]
 });
 
 /* Validate configuration with loaded schema */
 config.validate({ allowed: "warn" });
+
+config.load({
+  repositories: [
+    {
+      name: "asterics-docs",
+      location: ""
+    },
+    ...config.get("submodules")
+  ]
+});
 
 module.exports = config;
