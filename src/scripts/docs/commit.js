614262788517989e6dce04b57251c6c69dcb9dc3
Alija Sabic <sabic.alija@gmail.com>
2019-06-24 23:10:50 +0200

    docs commit: process commit message

--- src/scripts/docs/commit.js
+++ src/scripts/docs/commit.js
@@ -6,13 +6,11 @@ import { error } from "./shared/logger.js";
 import { ask, hasUntracked, resolveUntracked, promptSelection } from "./shared/untracked.js";
 import { Index } from "./shared/index.js";
 import { getStaged, getUnstaged, getUntracked } from "./shared/status.js";
-// import { getIndex } from "./shared/util.js";
 import { getBranchesOfRepository } from "./shared/util.js";
 import { mkdirp } from "@asterics/node-utils";
 
 const configPath = join(process.cwd(), "src/config/config.js");
 const config = require(configPath);
-// const repositories = config.get("repositories");
 const submodules = config.get("submodules");
 let index = null;
 
@@ -33,12 +31,18 @@ export const builder = yargs => {
   });
 };
 export const handler = async options => {
-  // console.log("commit", options);
   try {
     /* Load current index */
     const dependendencies = config.get("dependencies");
     const versions = config.get("versions");
     index = await new Index(dependendencies, versions);
+
+    /* Get commit message */
+    if (typeof options.message === "undefined") {
+      do {
+        options.message = options.m = await ask("\nPlease provide a commit message: ");
+      } while (typeof options.message === "undefined");
+    }
 
     /* Verify if author, committer, and message are specified */
     if (!verifyOptions(options)) {
@@ -57,10 +61,6 @@ export const handler = async options => {
     });
 
     /* Verify if any new files were created */
-    // while (await hasUntracked(index)) {
-    //   await resolveUntracked(index);
-    // }
-
     if (await hasUntracked(index)) {
       do {
         await resolveUntracked(index);
@@ -100,8 +100,6 @@ async function commit(index, options) {
   const r = await Repository.open(d);
   const staged = await getStaged(r);
 
-  // console.log("\n");
-
   // Iterate repo
   for (const repo of submodules) {
     const b = await getBranchesOfRepository(repo.name);
@@ -117,8 +115,6 @@ async function commit(index, options) {
 
 async function commitFiles(index, repo, branch, status, options) {
   try {
-    /* TODO: Get commit message */
-
     /* Open repository */
     const location = join(process.cwd(), repo.location);
     const r = await Repository.open(location);
@@ -150,8 +146,6 @@ async function commitFiles(index, repo, branch, status, options) {
         /* Delete old file */
         const oldDest = join(process.cwd(), repo.location, entry.source, entry.file);
         if (existsSync(oldDest)) unlinkSync(oldDest);
-
-        /* TODO: Handle case for NEW file */
       } else {
         src = join(process.cwd(), config.get("documentation"), file.path());
         dest = join(process.cwd(), repo.location, entry.source, entry.file);
@@ -173,7 +167,7 @@ async function commitFiles(index, repo, branch, status, options) {
     /* Commit */
     const author = Signature.now(getName(options.author), getEmail(options.author));
     const committer = Signature.now(getName(options.committer), getEmail(options.committer));
-    await r.createCommit("HEAD", author, committer, "new commit", oid, [parent]);
+    await r.createCommit("HEAD", author, committer, options.message, oid, [parent]);
   } catch (err) {
     console.log(err);
   }
@@ -183,12 +177,12 @@ async function checkoutBranch(name, repo, branch) {
   const opts = {
     checkoutStrategy:
       Checkout.STRATEGY.SAFE || Checkout.STRATEGY.RECREATE_MISSING || Checkout.STRATEGY.ALLOW_CONFLICTS || Checkout.STRATEGY.USE_THEIRS,
-    progressCb: (path, checkedOut, total) => {
+    progressCb: (path, checkedOut, total, one, two, three) => {
       if (total === 0) return;
       const prefix = "Checking out files: ";
       const percent = Math.round((checkedOut / total) * 100);
 
-      if (percent === 0) {
+      if (checkedOut === 0) {
         process.stdout.write(prefix);
       } else {
         clearScreenDown(process.stdout);
@@ -229,39 +223,6 @@ async function checkoutBranch(name, repo, branch) {
   /* Set HEAD to branch */
   await repo.setHead(`refs/heads/${branch}`);
 }
-
-// async function checkoutBranch(repo, branch) {
-//   const opts = {
-//     checkoutStrategy:
-//       Checkout.STRATEGY.SAFE || Checkout.STRATEGY.RECREATE_MISSING || Checkout.STRATEGY.ALLOW_CONFLICTS || Checkout.STRATEGY.USE_THEIRS
-//   };
-//   const remote = await repo.getBranch(`origin/${branch}`);
-//   const commit = await repo.getCommit(remote.target());
-//   const tree = await repo.getTree(commit.treeId());
-//   await Checkout.tree(repo, tree, opts);
-//   await repo.setHead(`refs/heads/${branch}`);
-// }
-
-function printIndexInfo(status) {
-  for (const file of status) {
-    let entry = index.entry(file);
-    process.stdout.write(`${file.path()}, ${entry.repository}, ${entry.branch}, ${join(entry.source, entry.file)}\n`);
-  }
-}
-
-// async function getStaged() {
-//   const d = join(process.cwd(), config.get("documentation"));
-//   const r = await Repository.open(d);
-//   const opts = {
-//     flags:
-//       Status.OPT.INCLUDE_UNTRACKED |
-//       Status.OPT.RECURSE_UNTRACKED_DIRS |
-//       Status.OPT.RENAMES_HEAD_TO_INDEX |
-//       Status.OPT.RENAMES_FROM_REWRITES,
-//     show: Status.SHOW.INDEX_ONLY
-//   };
-//   return await r.getStatusExt(opts);
-// }
 
 function getStagedBranches(repo, branches, status) {
   let stagedBranches = status
