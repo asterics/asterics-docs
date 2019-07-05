127e5553882a3faac813d75c38c8749a46e779f9
Alija Sabic <sabic.alija@gmail.com>
2019-07-05 08:52:49 +0200

    @asterics/vuepress-theme-docs: add draft implementation for BreadCrumb.vue

---
+++ src/vuepress/themes/vuepress-theme-docs/components/BreadCrumb.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-container>
+    <b-row>
+      <div class="col-12 breadcrumb-container">
+        <b-breadcrumb :items="items"></b-breadcrumb>
+      </div>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          text: "Admin",
+          href: "#"
+        },
+        {
+          text: "Manage",
+          href: "#"
+        },
+        {
+          text: "Library",
+          active: true
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.breadcrumb-container {
+  margin-top: 3.5rem;
+  position: fixed;
+  z-index: 999;
+  display: inline-block;
+  padding-bottom: 2rem;
+  // padding: 2rem;
+  // padding: 0;
+}
+</style>
