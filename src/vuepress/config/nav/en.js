78a774e656fd004aca018a30cf0b6984a9f77c45
Martin Deinhofer <deinhofer.m@gmail.com>
2022-01-18 08:14:26 +0100

    moved Website editors guide, and developer guide to Documentation submenu

--- src/vuepress/config/nav/en.js
+++ src/vuepress/config/nav/en.js
@@ -3,43 +3,44 @@ module.exports = [
   { text: "Solutions", link: "/solutions/" },
   { text: "Tutorials", link: "/customize/" },
   { text: "Plugins", link: "/plugins/" },
-  {
-    text: "Web Applications",
-    items: [
-      {
-        text: "AsTeRICS Grid",
-        link: "https://grid.asterics.eu",
-      },
-      {
-        text: "WebACS",
-        link: "http://webacs.asterics.eu/?areBaseURI=http://127.0.0.1:8081",
-      },
-      {
-        text: "FABI WebUI",
-        link: "https://fabi.asterics.eu",
-      },
-      {
-        text: "FLipMouse WebUI",
-        link: "https://flipmouse.asterics.eu",
-      }
-    ],
-  },
-  {
-    text: "Manuals",
-    items: [
-      { text: "AsTeRICS Grid", link: "/manuals/asterics-grid/" },
-      { text: "WebACS", link: "/manuals/WebACS/" },
-      { text: "ACS", link: "/manuals/ACS/" },
-      // { text: "ARE", link: "/manuals/ARE/" }
-    ],
-  },
-  { text: "Website Editors Guide", link: "/guide/editor.html" },
+
   {
     text: "More",
     items: [
       {
-        text: "Developer Guide",
-        link: "/develop/",
+        text: "Web Applications",
+        items: [
+          {
+            text: "AsTeRICS Grid",
+            link: "https://grid.asterics.eu",
+          },
+          {
+            text: "WebACS",
+            link: "http://webacs.asterics.eu/?areBaseURI=http://127.0.0.1:8081",
+          },
+          {
+            text: "FABI WebUI",
+            link: "https://fabi.asterics.eu",
+          },
+          {
+            text: "FLipMouse WebUI",
+            link: "https://flipmouse.asterics.eu",
+          }
+        ],
+      },
+      {
+        text: "Documentation",
+        items: [
+          { text: "AsTeRICS Grid User Manual", link: "/manuals/asterics-grid/" },
+          { text: "WebACS User Manual", link: "/manuals/WebACS/" },
+          { text: "ACS User Manual", link: "/manuals/ACS/" },
+          // { text: "ARE", link: "/manuals/ARE/" }
+          { text: "Website Editors Guide", link: "/guide/editor.html" },
+          {
+            text: "Developer Guide",
+            link: "/develop/",
+          },
+        ],
       },
       {
         text: "Get Involved",
@@ -49,7 +50,7 @@ module.exports = [
           { text: "Contribute", link: "/get-involved/Contribute" },
           { text: "Legal Notice", link: "/get-involved/Legal-Notice" },
         ],
-      },
+      },    
     ],
   },
   // as long as there is just 1 language, comment it out
