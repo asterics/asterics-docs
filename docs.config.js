65137a1972c79b5b65c480cd7d38995e02271506
Alija Sabic <sabic.alija@gmail.com>
2022-11-23 15:01:54 +0100

    Update asterics-docs configuration: remove reference for asterics-docs itself

--- docs.config.js
+++ docs.config.js
@@ -9,7 +9,7 @@ module.exports = {
   sources: [
     {
       repository: "asterics-docs",
-      reference: ".",
+      // reference: ".",
       files: [
         {
           source: "src/vuepress",
